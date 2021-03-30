12000919cad0993e3576668fb7df3e95126bb41e
Jakub R <kubarob13@gmail.com>
2021-03-30 14:14:16 +0200

    Papugi malują fugi

--- L2/L2.py
+++ L2/L2.py
@@ -1,8 +1,14 @@
+#%% Imports
+
+import os
+
 import cv2
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 #%% Functions
+
+
 def cv_imshow(img, img_title="image"):
     if (img.dtype == np.float32) or (img.dtype == np.float64):
         img_ = img / 255
@@ -10,6 +16,12 @@ def cv_imshow(img, img_title="image"):
         img_ = img
     cv2.imshow(img_title, img_)
     cv2.waitKey(1)
+
+
+def cv_imwrite(path, name, image):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    cv2.imwrite(path + "/" + name, image)
 
 
 def calcPSNR(img1, img2):
@@ -23,25 +35,46 @@ def printi(img, img_title="image"):
 
 
 #%% Resources
-image = cv2.imread("parrots_col.png", cv2.IMREAD_UNCHANGED)
-image_noise = cv2.imread("parrots_col_noise.png", cv2.IMREAD_UNCHANGED)
-image_inoise = cv2.imread("parrots_col_inoise.png", cv2.IMREAD_UNCHANGED)
+IMAGE = cv2.imread("parrots_col.png", cv2.IMREAD_UNCHANGED)
+IMAGE_NOISE = cv2.imread("parrots_col_noise.png", cv2.IMREAD_UNCHANGED)
+IMAGE_INOISE = cv2.imread("parrots_col_inoise.png", cv2.IMREAD_UNCHANGED)
+
+NOISE = [IMAGE_NOISE, IMAGE_INOISE]
+save_dir = "/out/"
 
 #%% Zad 1
-for img_noise in [image_noise, image_inoise]:
-    print("\nFiltr Gaussa")
+NOISE_DESC = ["Szum Gaussa", "Szum Medianowy"]
+for j in range(len(NOISE)):
+    print(f"{NOISE_DESC[j]}: Filtr Gaussa")
     for i in [3, 5, 7]:
-        gblur_img = cv2.GaussianBlur(img_noise, (i, i), 0)
-        print(f"Maska {i}x{i}: PSNR = {calcPSNR(image, gblur_img)}")
-    print("\nFiltr Medianowy")
+        gblur_img = cv2.GaussianBlur(NOISE[j], (i, i), 0)
+        cv_imwrite("out/Z1", f"szum{j}_gauss{i}x{i}.png", gblur_img)
+        print(f"Maska {i}x{i}: PSNR = {calcPSNR(IMAGE, gblur_img)}")
+    print(f"\n{NOISE_DESC[j]}: Filtr Medianowy")
     for i in [3, 5, 7]:
-        median_img = cv2.medianBlur(img_noise, i)
-        print(f"Maska {i}x{i}: PSNR = {calcPSNR(image, gblur_img)}")
+        median_img = cv2.medianBlur(NOISE[j], i)
+        cv_imwrite("out/Z1", f"szum{j}_median{i}x{i}.png", median_img)
+        print(f"Maska {i}x{i}: PSNR = {calcPSNR(IMAGE, median_img)}")
+    print()
 
 #%% Zad 2
+IMAGE_CONVERTED = cv2.cvtColor(IMAGE, cv2.COLOR_BGR2YCrCb)
 
-""" Here be dragons """
+NEW_IMAGE = np.zeros(IMAGE_CONVERTED.shape, dtype=IMAGE_CONVERTED.dtype)
+
+NEW_IMAGE[:, :, 0] = cv2.equalizeHist(IMAGE_CONVERTED[:, :, 0])
+NEW_IMAGE[:, :, 1] = IMAGE_CONVERTED[:, :, 1]
+NEW_IMAGE[:, :, 2] = IMAGE_CONVERTED[:, :, 2]
+NEW_IMAGE = cv2.cvtColor(NEW_IMAGE, cv2.COLOR_YCrCb2BGR)
+
+cv_imshow(IMAGE, "Obraz bazowy")
+cv_imwrite(f"out/Z2", f"Bazowy.png", IMAGE)
+cv_imshow(NEW_IMAGE, "Histogram")
+cv_imwrite("out/Z2", "PoNormalizacji.png", NEW_IMAGE)
 
 #%% Zad 3
 
-""" Here be dragons """
+"""Hic sunt dracones"""
+
+#%% Stop
+cv2.waitKey(0)
